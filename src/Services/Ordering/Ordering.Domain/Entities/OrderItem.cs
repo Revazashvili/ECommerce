1c805c05eaf8c6592496380df22eaec3e086949f
Revazashvili <levanrevazashvili@gmail.com>
2024-12-08 11:58:33 +0400

    remove redundant ctors in OrderItem.cs and make properties required

--- src/Services/Ordering/Ordering.Domain/Entities/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/OrderItem.cs
@@ -5,32 +5,29 @@ namespace Ordering.Domain.Entities;
 
 public class OrderItem : Entity
 {
-    private OrderItem() { }
-    private OrderItem(Guid productId, string productName, decimal price, int quantity, string pictureUrl)
-    {
-        if (quantity <= 0)
-            throw new InvalidQuantityException();
-        
-        ProductId = productId;
-        ProductName = productName;
-        Price = price;
-        Quantity = quantity;
-        PictureUrl = pictureUrl;
-    }
-
     public static OrderItem Create(Guid productId,
         string productName,decimal price,
         int quantity, string pictureUrl)
     {
-        return new OrderItem(productId, productName, price, quantity, pictureUrl);
+        if (quantity <= 0)
+            throw new InvalidQuantityException();
+
+        return new OrderItem
+        {
+            ProductId = productId,
+            ProductName = productName,
+            Price = price,
+            Quantity = quantity,
+            PictureUrl = pictureUrl
+        };
     }
     
-    public Guid Id { get; set; }
-    public Guid ProductId { get; }
-    public string ProductName { get; }
-    public decimal Price { get; }
-    public int Quantity { get; private set; }
-    public string PictureUrl { get; }
+    public Guid Id { get; init; }
+    public required Guid ProductId { get; init; }
+    public required string ProductName { get; init;  }
+    public required decimal Price { get; init; }
+    public required int Quantity { get; set; }
+    public required string PictureUrl { get; init; }
 
     public void IncreaseQuantity(int quantityToAdd)
     {
