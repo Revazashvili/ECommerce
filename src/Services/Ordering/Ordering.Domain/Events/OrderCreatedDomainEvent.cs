36195a7e9b99db06d7b0b1f91996e619d7a61b23
Revazashvili <levanrevazashvili@gmail.com>
2025-02-20 16:10:03 +0400

    remove OrderCreatedDomainEvent.cs

--- src/Services/Ordering/Ordering.Domain/Events/OrderCreatedDomainEvent.cs
+++
@@ -1,8 +0,0 @@
-using Contracts.Mediatr.Wrappers;
-
-namespace Ordering.Domain.Events;
-
-public class OrderCreatedDomainEvent(int userId) : INotification
-{
-    public int UserId { get; } = userId;
-}
