6f7aea7be78288502ef37f5d6197afc405d527d8
Revazashvili <levanrevazashvili@gmail.com>
2025-02-21 12:02:15 +0400

    remove unused constructors from OrderingException.cs

--- src/Services/Ordering/Ordering.Domain/Exceptions/OrderingException.cs
+++ src/Services/Ordering/Ordering.Domain/Exceptions/OrderingException.cs
@@ -2,14 +2,7 @@ namespace Ordering.Domain.Exceptions;
 
 public class OrderingException : Exception
 {
-    public OrderingException()
-    { }
-
     public OrderingException(string message)
         : base(message)
-    { }
-
-    public OrderingException(string message, Exception innerException)
-        : base(message, innerException)
     { }
 }
