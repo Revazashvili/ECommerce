5db7f515e483909d30e67e7691735d6165e98345
Revazashvili <levanrevazashvili@gmail.com>
2025-02-22 18:53:51 +0400

    make Outbox const internal

--- src/Services/Ordering/Ordering.Infrastructure/EntityConfigurations/Schema.cs
+++ src/Services/Ordering/Ordering.Infrastructure/EntityConfigurations/Schema.cs
@@ -3,5 +3,5 @@ namespace Ordering.Infrastructure;
 internal static class Schema
 {
     internal const string Order = "orders";
-    public const string Outbox = "outbox";
+    internal const string Outbox = "outbox";
 }
