e5e2a76455c736f1de267be70d87aaacafe407cf
Revazashvili <levanrevazashvili@gmail.com>
2025-02-25 20:09:06 +0400

    remove unused db-creation.sql from everywhere

--- src/Services/Ordering/Tooling/db_creation.sql
+++
@@ -1,9 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'OrderingDB')
-    BEGIN
-        CREATE DATABASE OrderingDB;
-        PRINT 'Database created successfully';
-    END
-ELSE
-    BEGIN
-        PRINT 'Database already exists';
-    END
